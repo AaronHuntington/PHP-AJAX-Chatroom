b82b85127a6c185f77a320c7b36de864b65add34
AaronHuntington <8ahuntington@gmail.com>
2016-12-11 16:40:21 -0600

    Worked on front of admin/index.php, chat tables

--- admin/processChatAdmin.php
+++ admin/processChatAdmin.php
@@ -5,54 +5,56 @@
     $switch = new chatroom_switch;
     $chat_switch = $switch->chatroom_switch_update();
     $chatrooms = $switch->chatrooms();
-
-
-
-    echo '<pre>';
-    // var_dump($chatrooms);
-
-    echo '<br><br><br>';
-
-    // foreach($chatrooms as $chatroom){
-    //     // var_dump($chatroom);
-    //     echo 'Name: '.$chatroom[1]."<br>";
-    //     echo 'Key: '.$chatroom[2]."<br>";
-    //     echo 'Switch: '.$chatroom[3]."<br>";
-    //     echo '<br>';
-    //     echo '======';
-    //     echo '<br>';
-    //     // echo $chatroom['key'];
-    // }
-
 ?>
 
     <div id="chatSwitch_container" class="col-md-10 col-md-offset-1 borderRed">
         <div id="chatSwitch_activeBox" class="col-md-6 borderGreen">
             <h3 class="text-center">Active Chatrooms</h3>
-            <?php
-                foreach($chatrooms as $chatroom){
-                    echo '<br>';
-                    echo 'Name: '.$chatroom[1]."<br>";
-                    echo 'Key: '.$chatroom[2]."<br>";
-                    echo 'Switch: '.$chatroom[3]."<br>";
-                    echo '<br>';
-                    echo '======';
-                    echo '<br>';
-                }
-            ?>
+            <table class="table table-hover">
+                <tr>
+                    <th>Name</th>
+                    <th>Key</th>
+                </tr>
+                <?php
+                    foreach($chatrooms as $chatroom){
+                        $guestName  = $chatroom[1];
+                        $key        = $chatroom[2];
+                        $switch     = $chatroom[3];
+
+                        if($switch == 1){
+                            echo '<tr>';
+                            echo '<td>'.$guestName.'</td>';
+                            echo '<td>'.$key.'</td>';
+                            echo '<td><a href="#">Set Inactive</a></td>';
+                            echo '</tr>';
+                        }
+                    }
+                ?>    
+            </table>
         </div>
         <div id="chatSwitch_inactiveBox" class="col-md-6 borderBlue">
             <h3 class="text-center">inActive Chatrooms</h3> 
-            <?php
-                foreach($chatrooms as $chatroom){
-                    echo 'Name: '.$chatroom[1]."<br>";
-                    echo 'Key: '.$chatroom[2]."<br>";
-                    echo 'Switch: '.$chatroom[3]."<br>";
-                    echo '<br>';
-                    echo '======';
-                    echo '<br>';
-                }
-            ?>
+            <table class="table table-hover">
+                <tr>
+                    <th>Name</th>
+                    <th>Key</th>
+                </tr>
+                <?php
+                    foreach($chatrooms as $chatroom){
+                        $guestName  = $chatroom[1];
+                        $key        = $chatroom[2];
+                        $switch     = $chatroom[3];
+
+                        if($switch == 0){
+                            echo '<tr>';
+                            echo '<td>'.$guestName.'</td>';
+                            echo '<td>'.$key.'</td>';
+                            echo '<td><a href="#">Delete</a></td>';
+                            echo '</tr>';
+                        }
+                    }
+                ?>    
+            </table>
         </div>
     </div>
 
--- includes/oop/chatroom_switch.php
+++ includes/oop/chatroom_switch.php
@@ -8,15 +8,11 @@
             $query = $database->query($sql);
 
             $data = array();
-
             while($row = mysqli_fetch_array($query)){
                 $data[] = $row;
             }
-
             return $data;
         }
-
-    
 
         function chatrooms(){
             global $database;
@@ -24,46 +20,10 @@
             $sql = "SELECT * FROM chatroom_switch";
             $query = $database->query($sql);
 
-            // $chatroom['guestName'] = array();
-            // $chatroom['key'] = array();
-            // $chatroom['switch'] = array();
-
-            // // while($row = mysqli_fetch_assoc($query)){
-            // while ($row = $query->fetch_all(PDO::FETCH_ASSOC)) {
-            //     $chatroom[]['guestName'] = $row[0];
-            //     $chatroom[]['key'] = $row[0];
-            //     $chatroom[]['switch'] = $row[0];
-
-            // }
-
             $result = $query->fetch_all(PDO::FETCH_ASSOC);
             return $result;
-
-            // return $chatroom;
         }
     }
-
-
-
-
-
-
-
-
-
-
-        // public function get_inbox(){
-        // global $database;
-
-        // $sql = "SELECT * FROM contact_form_inbox";
-        // $query = $database->query($sql);
-
-        // $data = array();
-        
-        // while($row = mysqli_fetch_array($query)){
-        //     $data[] = $row;
-        // }
-        // return $data;
     
 
 ?>
