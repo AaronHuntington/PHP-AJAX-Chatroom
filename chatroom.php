03be5652c3b580e552eec081e42c44a254d3fd4c
AaronHuntington <8ahuntington@gmail.com>
2016-12-13 10:49:49 -0600

    Working on chatroom messages

--- chatroom.php
+++ chatroom.php
@@ -4,23 +4,41 @@
 
     $key_generator = new chatroom;
 
-
-
-
-
     if(isset($_GET['guest']) && isset($_GET['susi']) === false){
         $guest = $_GET['guest'];
         $key_generator->key_generator($guest);
     }
 
-
-
-
+    $guest  = $_GET['guest'];
+    $key    = $_GET['susi'];
 ?>
+
+<script type="text/javascript">
+    setInterval(function(){
+        alert('we');
+    },1000);//Updates the chat 1000 millisecond
+
+
+</script>
+
     <section class="row">
         <div class="col-md-8 col-md-offset-2">
             <h1 class="text-center">Quack</h1>
             <div id="chat_container" class="col-md-10 col-md-offset-1 borderRed">
+                <h3>Greetings <?php echo $guest;?>! We will be right with you shortly.</h3>
+                <?php
+
+
+
+                
+
+
+
+
+
+
+
+                ?>
             </div>
             <div id="chat_entry_container" class="col-md-10 col-md-offset-1 borderRed">
                 <?php include(INCLUDES_ROOTPATH."chat_entryForm.php"); ?> 
--- includes/chat_entryForm.php
+++ includes/chat_entryForm.php
@@ -7,8 +7,8 @@
 ?>
 
 
-<input id="chatKey" type="hidden" value="12345"/>
-<input id="chatName" type="hidden" value="Aaron Test"/>
+<input id="chatKey" type="hidden" value="<?php echo $key; ?>"/>
+<input id="chatName" type="hidden" value="<?php echo $guest;?>"/>
 <input id="message" type="text" placeholder="Enter Message" class="col-md-10"/>
 <div class="col-md-2">
     <input type="submit" id="chatroom_sendbtn" onclick="sendMessage()"value="Send"/>
